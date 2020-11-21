0bc4d7a32ebc4176bec3f87a0910d43fc92c0499
schettn <nicoschett@icloud.com>
2020-11-21 22:42:01 +0100

    Extend track app
    * The track audio file now has validators.
    * A invalid stream value key in track tags has been fixed.
    * Pagination has been added to PAC and improved for Track. (#7)
    * GraphQL fields have been added for PAC and Track.
    * Wagtailadmin panels have been added for PAC and Track.
    * PAC field has been added as search fields for Track.
    * The audio file url can now be accessed via GraphQL.
    * Registered TagBlock in bifrost.
    * Register Track and PAC in wagtailadmin.

--- esite/track/blocks.py
+++ esite/track/blocks.py
@@ -1,8 +1,17 @@
 from wagtail.core import blocks
+from esite.bifrost.helpers import register_streamfield_block
+from esite.bifrost.models import (
+    GraphQLString,
+)
 
 
+@register_streamfield_block
 class TagBlock(blocks.StructBlock):
     name = blocks.CharBlock(required=True, max_length=16)
+
+    graphql_fields = [
+        GraphQLString("name"),
+    ]
 
 
 # SPDX-License-Identifier: (EUPL-1.2)
---
+++ esite/track/validators.py
@@ -0,0 +1,43 @@
+import os
+from django.core.exceptions import ValidationError
+
+
+def validate_audio_file(value):
+    if value._committed:
+        return True
+
+    file = value._file
+    if file:
+        if file.size > 12 * 1024 * 1024:
+            raise ValidationError("Audio file too large ( > 12mb )")
+        if not file.content_type in [
+            "audio/mpeg",
+            "audio/mp4",
+            "audio/basic",
+            "audio/x-midi",
+            "audio/vorbis",
+            "audio/x-pn-realaudio",
+            "audio/vnd.rn-realaudio",
+            "audio/x-pn-realaudio",
+            "audio/vnd.rn-realaudio",
+            "audio/wav",
+            "audio/x-wav",
+        ]:
+            raise ValidationError(
+                "Sorry, we do not support that audio MIME type. Please try uploading an mp3 file, or other common audio type."
+            )
+        if not os.path.splitext(file._name)[1] in [
+            ".mp3",
+            ".au",
+            ".midi",
+            ".ogg",
+            ".ra",
+            ".ram",
+            ".wav",
+        ]:
+            raise ValidationError(
+                "Sorry, your audio file doesn't have a proper extension."
+            )
+        return file
+    else:
+        raise ValidationError("Couldn't read uploaded file")
