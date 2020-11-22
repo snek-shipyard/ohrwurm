d4cd52ddeb760ee1e95ccc62a9aa1d6f5a0de8a5
Schett Nico <52858351+schettn@users.noreply.github.com>
2020-11-22 02:07:13 +0100

    Revert "Add track app (#9)" (#10)
    This reverts commit e45f4869171129e835990b2365e821860e29973b.

--- esite/track/wagtail_hooks.py
+++
@@ -1,8 +0,0 @@
-from django.templatetags.static import static
-from django.utils.html import format_html
-
-from wagtail.core import hooks
-
-
-# SPDX-License-Identifier: (EUPL-1.2)
-# Copyright © 2019-2020 Simon Prast
--- esite/user/admin.py
+++ esite/user/admin.py
@@ -6,10 +6,18 @@ from wagtail.contrib.modeladmin.options import (
     modeladmin_register,
 )
 
+# from esite.enterprises.admin import EnterpriseAdmin
+# from esite.people.admin import PersonAdmin
+# from esite.profile.admin import ProfileAdmin
+# from esite.registration.admin import RegistrationAdmin
+
+# Register your user related models here.
+
+
 class UserAdmin(ModelAdmin):
     model = get_user_model()
     menu_label = "Person"
-    # menu_icon = "user"
+    menu_icon = "user"
     menu_order = 290
     add_to_settings_menu = False
     exclude_from_explorer = False
@@ -21,14 +29,14 @@ class UserAdmin(ModelAdmin):
 
 class UserManagementAdmin(ModelAdminGroup):
     menu_label = "User Management"
-    # menu_icon = "group"
+    menu_icon = "group"
     menu_order = 110
     add_to_settings_menu = False
     exclude_from_explorer = False
-    items = (UserAdmin,)
+    # items = (ProfileAdmin, PersonAdmin, EnterpriseAdmin, RegistrationAdmin)
 
 
-# modeladmin_register(UserManagementAdmin)
+modeladmin_register(UserManagementAdmin)
 
 # SPDX-License-Identifier: (EUPL-1.2)
 # Copyright © 2019-2020 Simon Prast
