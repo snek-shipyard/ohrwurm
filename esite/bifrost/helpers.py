e45f4869171129e835990b2365e821860e29973b
Schett Nico <52858351+schettn@users.noreply.github.com>
2020-11-22 01:09:01 +0100

    Add track app (#9)
    * Add pagination support for Bifrost
    
    Now Bifrost support pagination.
    Ref: https://github.com/GrappleGQL/wagtail-grapple/
    
    * Update wagtail core middleware import
    
    Due to v2.11.2 the SiteMiddleware is deprecated.
    Therefore the legacy variate is used.
    Ref: https://docs.wagtail.io/en/stable/releases/2.11.html
    
    * Add track app
    
    Track and ProjectAudioChannel model have been added.
    Also the track model is registered in bifrost with following
    extras: Pagination, Search.
    
    * Register track app
    
    The track app is now registered in INSTALLED_APPS
    and BIFROST_APPS.
    
    * Fix invalid settings
    
    * Adjusted settings in order to provide search functionality.
    
    * Extend track app
    
    - The track audio file now has validators.
    - A invalid stream value key in track tags has been fixed.
    - Pagination has been added to PAC and improved for Track. (#7)
    - GraphQL fields have been added for PAC and Track.
    - Wagtailadmin panels have been added for PAC and Track.
    - PAC field has been added as search fields for Track.
    - The audio file url can now be accessed via GraphQL.
    - Registered TagBlock in bifrost.
    - Register Track and PAC in wagtailadmin.
    
    * Fix error in user app admin registration (#6)
    
    - Wagtailadmin is accessible now.
    - Code of user admin has been cleaned.
    
    * Implement extended bifrost decorators (#8)
    
    - When using bifrosts query field decorators it is now
    possible to filter lists.

--- esite/bifrost/helpers.py
+++ esite/bifrost/helpers.py
@@ -66,6 +66,17 @@ def register_query_field(field_name, plural_field_name=None, query_params=None):
                 if issubclass(cls, Page):
                     qs = cls.objects.live().public().order_by("-first_published_at")
 
+                # Filter queryset for field_query_params
+                # Important: each field_query_param must be indexed in the model:
+                # index.FilterField("field_query_param")
+                qs = qs.filter(
+                    **{
+                        key: kwargs.get(key)
+                        for (key, value) in field_query_params.items()
+                        if key is not "id"
+                    }
+                )
+
                 return resolve_queryset(qs.all(), info, **kwargs)
 
             # Create schema and add resolve methods
@@ -85,6 +96,118 @@ def register_query_field(field_name, plural_field_name=None, query_params=None):
             return schema
 
         # Send schema to Bifrost schema.
+        register_graphql_schema(Mixin())
+        return cls
+
+    return inner
+
+
+def register_paginated_query_field(
+    field_name,
+    plural_field_name=None,
+    query_params=None,
+    required=False,
+    plural_required=False,
+    plural_item_required=False,
+):
+    from .types.structures import PaginatedQuerySet
+    from .utils import resolve_paginated_queryset
+
+    if not plural_field_name:
+        plural_field_name = field_name + "s"
+
+    def inner(cls):
+        field_type = lambda: registry.models[cls]
+        field_query_params = query_params
+        if field_query_params is None:
+            field_query_params = {"id": graphene.Int()}
+
+            if issubclass(cls, Page):
+                field_query_params["slug"] = graphene.Argument(
+                    graphene.String, description=ugettext_lazy("The page slug.")
+                )
+                field_query_params["token"] = graphene.Argument(
+                    graphene.String, description=ugettext_lazy("The preview token.")
+                )
+
+        def Mixin():
+            # Generic methods to get all and query one model instance.
+            def resolve_singular(self, _, info, **kwargs):
+                # If no filters then return nothing.
+                if not kwargs:
+                    return None
+
+                try:
+                    # If is a Page then only query live/public pages.
+                    if issubclass(cls, Page):
+                        if "token" in kwargs and hasattr(
+                            cls, "get_page_from_preview_token"
+                        ):
+                            return cls.get_page_from_preview_token(kwargs["token"])
+
+                        return cls.objects.live().public().get(**kwargs)
+
+                    return cls.objects.get(**kwargs)
+                except:
+                    return None
+
+            def resolve_plural(self, _, info, **kwargs):
+                qs = cls.objects
+                if issubclass(cls, Page):
+                    qs = qs.live().public().order_by("-first_published_at")
+
+                # Filter queryset for field_query_params
+                # Important: each field_query_param must be indexed in the model:
+                # index.FilterField("field_query_param")
+                qs = qs.filter(
+                    **{
+                        key: kwargs.get(key)
+                        for (key, value) in field_query_params.items()
+                        if key is not "id"
+                    }
+                )
+
+                return resolve_paginated_queryset(qs.all(), info, **kwargs)
+
+            # Create schema and add resolve methods
+            schema = type(cls.__name__ + "Query", (), {})
+
+            singular_field_type = field_type
+            if required:
+                singular_field_type = graphene.NonNull(field_type)
+
+            setattr(
+                schema,
+                field_name,
+                graphene.Field(singular_field_type, **field_query_params),
+            )
+
+            plural_field_type = field_type
+            if plural_item_required:
+                plural_field_type = graphene.NonNull(field_type)
+
+            setattr(
+                schema,
+                plural_field_name,
+                PaginatedQuerySet(
+                    plural_field_type,
+                    cls,
+                    required=plural_required,
+                    **field_query_params
+                ),
+            )
+
+            setattr(
+                schema, "resolve_" + field_name, MethodType(resolve_singular, schema)
+            )
+            setattr(
+                schema,
+                "resolve_" + plural_field_name,
+                MethodType(resolve_plural, schema),
+            )
+            return schema
+
+        # Send schema to Grapple schema.
         register_graphql_schema(Mixin())
         return cls
 
