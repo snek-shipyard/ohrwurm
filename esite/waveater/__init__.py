57ac9ecf465b20998579815b21ad98639c407075
schettn <nicoschett@icloud.com>
2020-12-28 11:08:37 +0100

    Fix double start of custom waveater config
    The development server is always started twice. Thus
    the waveater thread too. This is now fixed.

--- esite/waveater/__init__.py
+++ esite/waveater/__init__.py
@@ -1,5 +1,9 @@
 # myapp/_init_.py
-default_app_config = "esite.waveater.apps.WaveaterConfig"
+import os
 
+if os.environ.get('RUN_MAIN', None) != 'true':
+    os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+    default_app_config = 'esite.waveater.apps.WaveaterConfig'
+    
 # SPDX-License-Identifier: (EUPL-1.2)
 # Copyright © 2020 miraculix-org Florian Kleber
